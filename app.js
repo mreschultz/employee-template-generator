0f093f41e7ec2448de2c3b98441a905ba00e63a4
Eric Schultz <mreschultz@me.com>
2020-10-26 22:05:25 -0400

    Added index.js file

--- app.js
+++
@@ -1,7 +0,0 @@
-const Manager = require("./lib/Manager");
-
-manager = new Manager(answers.name, answer.id, answer.email);
-
-const manager = new Manager(answers.name, answer.id, answer.email);
-
-teamZArray.push(manager);
